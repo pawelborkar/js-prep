1ba5f817f54996337b233b670c6e3a7ab8d9952c
Pawel Borkar <pawelsb@zohomail.com>
2022-05-02 15:33:46 +0530

    Refactor

--- Currying/infinite-currying.js
+++ Currying/infinite-currying.js
@@ -9,11 +9,6 @@ console.log(add(10)(12)(100)(10)(12)());
 
 // -------------------------------------------
 // Using Arrow function
+const add1 = (param1) => (param2) => b ? add1(param1 + param2) : param1;
 
-const add = (param1) => (param2) => {
-
-    if (param2) return add(param1 + param2);
-    return param1;
-};
-
-console.log(add(10)(12)(100)(10)(12)());
+console.log(add(10)(12)(10)(10)(12)());
